142de545e69eb5b919e98794c71111bbb4ea3200
Naoures Tahri <naoures.tahri@esprit.tn>
2023-10-15 01:33:02 +0100

    new class User.java by beature1 branch

---
+++ src/main/java/tn/esprit/spring/kaddem/entities/User.java
@@ -0,0 +1,16 @@
+package tn.esprit.spring.kaddem.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer idUser;
+    private String nomUser;
+}
